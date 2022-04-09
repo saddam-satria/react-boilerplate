2da5c0686f5609a6363b6d7539c7d7c32130f68b
saddam-satria <ardhisaddam@gmail.com>
2022-04-09 23:00:23 +0700

    add routing router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,9 @@
 import React from 'react';
+import { Routing } from './routing/routes';
 import './style/app.scss';
 
 const App = () => {
-    return (
-        <>
-            <div className={'container'}>
-                <div className={'flex'}>
-                    <img alt="" src="react-icon.png" className={'react-logo'} />
-                    <div style={{ padding: '10px 0px' }} className={'flex'}>
-                        <span
-                            style={{
-                                color: 'white',
-                                fontSize: '36px',
-                            }}
-                        >
-                            edit src/app.jsx
-                        </span>
-                        <span
-                            style={{
-                                color: 'white',
-                                fontSize: '18px',
-                                padding: '5px 0px',
-                                textTransform: 'lowercase',
-                            }}
-                        >
-                            Created by{' '}
-                            <span style={{ fontWeight: 700 }}>Saddam</span>
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+    return <Routing />;
 };
 
 export default App;
