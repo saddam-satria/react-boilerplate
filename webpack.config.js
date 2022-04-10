4b790190167a8495ea5f35a68ac154dd4b393f7d
saddam-satria <ardhisaddam@gmail.com>
2022-04-10 13:59:38 +0700

    update historycallback

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,80 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const rules = [
-    {
-        test: /\.(css|scss)$/i,
-        use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader',
+  {
+    test: /\.(css|scss)$/i,
+    use: [
+      'style-loader',
+      'css-loader',
+      'sass-loader',
 
-            { loader: 'postcss-loader' },
-        ],
+      { loader: 'postcss-loader' },
+    ],
+  },
+  {
+    test: /\.(jsx|js|ts|tsx)$/i,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
     },
-    {
-        test: /\.(jsx|js|ts|tsx)$/i,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-        },
-    },
-    {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        use: [
-            'url-loader',
-            {
-                loader: 'file-loader',
-            },
-        ],
-    },
-    {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-    },
-    {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-    },
-    {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-    },
+  },
+  {
+    test: /\.(png|jpg|jpeg|gif)$/i,
+    type: 'asset/resource',
+    use: [
+      'url-loader',
+      {
+        loader: 'file-loader',
+      },
+    ],
+  },
+  {
+    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+    type: 'asset/resource',
+  },
+  {
+    test: /\.(csv|tsv)$/i,
+    use: ['csv-loader'],
+  },
+  {
+    test: /\.xml$/i,
+    use: ['xml-loader'],
+  },
 ];
 
 module.exports = {
-    target: 'web',
-    entry: './src/index.js',
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: '[contenthash].bundle.js',
+  target: 'web',
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[contenthash].bundle.js',
+    publicPath: 'auto',
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.ts', '.tsx'],
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 3000,
+    server: 'http',
+    liveReload: true,
+    compress: true,
+    headers: {
+      'X-Custom-Author': 'saddam',
     },
-    resolve: {
-        extensions: ['.jsx', '.js', '.ts', '.tsx'],
+    client: {
+      logging: 'log',
     },
-    devtool: 'source-map',
-    devServer: {
-        port: 3000,
-        server: 'http',
-        liveReload: true,
-        compress: true,
-        headers: {
-            'X-Custom-Author': 'saddam',
-        },
-        client: {
-            logging: 'log',
-        },
-        hot: false,
-    },
-    plugins: [
-        new htmlWebpackPlugin({
-            filename: 'index.html',
-            title: 'react boilerplate',
-            template: './public/index.html',
-        }),
-    ],
-    module: { rules },
+    hot: false,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: 'index.html',
+      title: 'react boilerplate',
+      template: './public/index.html',
+    }),
+  ],
+  module: { rules },
 };
