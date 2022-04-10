fe87da5e6a9e1b90b782f04d9b92543b8e0e1ff4
saddam-satria <ardhisaddam@gmail.com>
2022-04-10 13:58:35 +0700

    add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,65 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/no-deprecated': 'warn',
+    'react/jsx-filename-extension': 'off',
+    'react/no-unknown-property': 'warn',
+    'react/require-render-return': 'warn',
+    'react/jsx-key': 'warn',
+    'react/jsx-no-target-blank': 'warn',
+    'react/jsx-uses-vars': 'warn',
+    'react/jsx-curly-brace-presence': 'off',
+    'no-console': 'off',
+    'no-duplicate-imports': 'warn',
+    'no-unused-vars': 'warn',
+    'comma-dangle': ['off', 'never'],
+    'import/order': [
+      'warn',
+      {
+        alphabetize: {
+          caseInsensitive: true,
+          order: 'asc',
         },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
+        groups: [
+          'builtin',
+          'external',
+          'index',
+          'sibling',
+          'parent',
+          'internal',
+        ],
+      },
     ],
-    "rules": {
-    }
-}
+    'jsx-quotes': ['warn', 'prefer-double'],
+    'object-curly-spacing': ['warn', 'always'],
+    'import/prefer-default-export': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
