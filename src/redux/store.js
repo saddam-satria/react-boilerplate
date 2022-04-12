155dd93691211edc1d1116cc9dc0bcab7dce9024
saddam-satria <ardhisaddam@gmail.com>
2022-04-12 19:48:11 +0700

    add redux

---
+++ src/redux/store.js
@@ -0,0 +1,19 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { reducers } from './reducers';
+
+/**
+ *
+ * @chromeExtension -> return redux dev tools
+ */
+
+const chromeExtension = () => {
+  return (
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+};
+
+export const store = createStore(
+  reducers,
+  compose(applyMiddleware(thunk), chromeExtension())
+);
