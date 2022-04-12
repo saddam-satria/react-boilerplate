a5cc7a13e7e790532e68f1d66ecf9bcbf4ae964e
saddam-satria <ardhisaddam@gmail.com>
2022-04-12 19:49:06 +0700

    update redux as container

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.scss';
+import Redux from './redux';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App />);
+root.render(
+  <Redux>
+    <App />
+  </Redux>
+);
